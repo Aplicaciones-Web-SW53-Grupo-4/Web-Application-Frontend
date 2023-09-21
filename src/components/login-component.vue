f4863f0042b0a777630f7776cee063c266136928
Erick130601 <104027427+Erick130601@users.noreply.github.com>
2023-09-21 13:05:52 -0500

    style: estilo de login Refs #2

--- src/components/login-component.vue
+++ src/components/login-component.vue
@@ -1,15 +1,31 @@
 <script setup>
-
+import VLazyImage from "v-lazy-image";
 </script>
 
-<template>
-  <pv-toolbar class="bg-primary-600 text-white font-bold text-3xl" role="navigation" aria-label="Toolbar">
-    <template #start>
-      <label> LOGIN</label>
+<template >
+  <div class="card">
+  <pv-toolbar class=" toolbar-1 text-white font-bold m-0 flex flex-wrap " role="navigation" aria-label="Toolbar">
+    <template #start >
+      <pv-images src="https://i.postimg.cc/rF47Qr56/ai-de-logo-Mesa-de-trabajo-1.png" alt="logo" width="100"/>
     </template>
   </pv-toolbar >
+
+    <div class="card flex justify-content-center">
+      <v-lazy-image src="https://i.postimg.cc/cCbb1f2m/pexels-pixabay-417192.jpg" />
+<!--      <pv-images src="https://i.postimg.cc/cCbb1f2m/pexels-pixabay-417192.jpg" alt="Image" width="1000" />-->
+    </div>
+
+  </div>
+
+
+
 </template>
 
 <style scoped>
+
+.toolbar-1{
+  background-color: #14131B;
+  padding:9px;
+}
 
 </style>
