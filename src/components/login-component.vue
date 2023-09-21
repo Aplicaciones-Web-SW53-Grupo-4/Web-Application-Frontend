13acf8061f960dd8c5bde10e8e0889a25b07ab25
Erick130601 <104027427+Erick130601@users.noreply.github.com>
2023-09-20 23:34:51 -0500

    refactor: estructura de componente login Reviewed-by: A Refs #1

--- src/components/login-component.vue
+++ src/components/login-component.vue
@@ -3,13 +3,11 @@
 </script>
 
 <template>
-
   <pv-toolbar class="bg-primary-600 text-white font-bold text-3xl" role="navigation" aria-label="Toolbar">
     <template #start>
       <label> LOGIN</label>
     </template>
   </pv-toolbar >
-
 </template>
 
 <style scoped>
