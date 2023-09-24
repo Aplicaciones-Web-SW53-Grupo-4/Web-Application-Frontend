40b302e9eb3a92b7ed26b6fac9faa9deac7248e4
Erick130601 <104027427+Erick130601@users.noreply.github.com>
2023-09-24 15:48:03 -0500

    shore and style : estructura y estilo de rent-tenant-major

--- src/router.js
+++ src/router.js
@@ -1,8 +1,9 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import registerTenantComponent from "@/components/login/Register-tenant-component.vue";
+import registerTenantComponent from "@/components/register/Register-tenant-component.vue";
 import loginTenantComponent from "@/components/login/login-tenant-component.vue";
 import loginOwnerComponent from "@/components/login/login-owner-component.vue";
-import registerOwnerComponent from "@/components/login/Register-owner-component.vue";
+import registerOwnerComponent from "@/components/register/Register-owner-component.vue";
+import rentTenantMajor from "@/components/rent/rent-tenant/rent-tenant-major.vue";
 
 
 
@@ -11,6 +12,7 @@ const routes = [
     { path: "/login-owner", component:loginOwnerComponent},
     { path: "/Register-tenant", component: registerTenantComponent},
     { path: "/Register-owner", component: registerOwnerComponent},
+    { path: "/rent-tenant-major", component: rentTenantMajor},
 ];
 
 const router = createRouter({
